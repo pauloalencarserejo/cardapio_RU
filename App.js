176c3ae83768ad4f85c704e776ba90ab7e3c9d90
Paulo Alencar <paulovictoralencar.13@gmail.com>
2020-03-03 20:57:50 -0300

    Inicio do projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Tu é o amor da minha vida! </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textDecorationColor: 'blue',
+  },
+});
