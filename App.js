1b68f379e739551cb05320c61b2a258c0e12eea9
Paulo Alencar <paulovictoralencar.13@gmail.com>
2020-03-05 16:33:50 -0300

    Excluindo pasta, e atualizando o cardapio

--- App.js
+++ App.js
@@ -8,47 +8,201 @@ import { TouchableHighlight } from 'react-native-gesture-handler';
 function Menu({ navigation }) {
   return (
     <View style={menu.View}>
-      <Button onPress={() => navigation.navigate('Segunda')}/>
+      <Button title='Segunda' style={menu.botao}onPress={() => navigation.navigate('Segunda')}/>
       
-      <Button onPress={() => navigation.navigate('Terca')}/>
+      <Button title='Terça' style={menu.botao} onPress={() => navigation.navigate('Terca')}/>
 
-      <Button onPress={() => navigation.navigate('Quarta')}/>
+      <Button title='Quarta' style={menu.botao} onPress={() => navigation.navigate('Quarta')}/>
       
-      <Button onPress={() => navigation.navigate('Quinta')} />
+      <Button title='Quinta' style={menu.botao} onPress={() => navigation.navigate('Quinta')} />
       
-      <Button style={menu.botao} onPress={() => navigation.navigate('Sexta')} />
-
-      </View>
+      <Button title='Sexta' style={menu.botao} onPress={() => navigation.navigate('Sexta')} />
+    </View>
   );
 }
 function Segunda(){
   return (
   <View>
     <Text>Segunda</Text>
+    <Text>Almoço</Text>
+    <Text>Principal</Text>
+    <Text>Assado de Panela Cubos de Frango ao Molho</Text>
+    <Text>Vegetariano</Text>
+    <Text>Fricassê de Grão de Bico</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Beterraba, Tomate e Abacaxi</Text>
+    <Text>Guarnição</Text>
+    <Text>Cuscuz</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Cajá</Text>
+    <Text>Sobremesa</Text>
+    <Text>Doce</Text>
+
+    <Text>Jantar</Text>
+    <Text>Principal</Text>
+    <Text>Carne Trinchada  Filé de Frango Grelhado</Text>
+    <Text>Vegetariano</Text>
+    <Text>Almôngedas de Soja</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Alface, Cenoura, Pepino e Milho</Text>
+    <Text>Guarnição</Text>
+    <Text>Macarrão Espaguete</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Manga</Text>
+    <Text>Sobremesa</Text>
+    <Text>Laranja Doce</Text>
   </View>) ;
 }
 function Terca(){
   return (
   <View>
     <Text>Terca</Text>
+    <Text>Almoço</Text>
+    <Text>Principal</Text>
+    <Text>Assado de Panela Cubos de Frango ao Molho</Text>
+    <Text>Vegetariano</Text>
+    <Text>Fricassê de Grão de Bico</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Beterraba, Tomate e Abacaxi</Text>
+    <Text>Guarnição</Text>
+    <Text>Cuscuz</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Cajá</Text>
+    <Text>Sobremesa</Text>
+    <Text>Doce</Text>
+
+    <Text>Jantar</Text>
+    <Text>Principal</Text>
+    <Text>Carne Trinchada  Filé de Frango Grelhado</Text>
+    <Text>Vegetariano</Text>
+    <Text>Almôngedas de Soja</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Alface, Cenoura, Pepino e Milho</Text>
+    <Text>Guarnição</Text>
+    <Text>Macarrão Espaguete</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Manga</Text>
+    <Text>Sobremesa</Text>
+    <Text>Laranja Doce</Text>
   </View>) ;
 }
 function Quarta(){
   return (
   <View>
     <Text>Quarta</Text>
+    <Text>Almoço</Text>
+    <Text>Principal</Text>
+    <Text>Assado de Panela Cubos de Frango ao Molho</Text>
+    <Text>Vegetariano</Text>
+    <Text>Fricassê de Grão de Bico</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Beterraba, Tomate e Abacaxi</Text>
+    <Text>Guarnição</Text>
+    <Text>Cuscuz</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Cajá</Text>
+    <Text>Sobremesa</Text>
+    <Text>Doce</Text>
+
+    <Text>Jantar</Text>
+    <Text>Principal</Text>
+    <Text>Carne Trinchada  Filé de Frango Grelhado</Text>
+    <Text>Vegetariano</Text>
+    <Text>Almôngedas de Soja</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Alface, Cenoura, Pepino e Milho</Text>
+    <Text>Guarnição</Text>
+    <Text>Macarrão Espaguete</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Manga</Text>
+    <Text>Sobremesa</Text>
+    <Text>Laranja Doce</Text>
   </View>) ;
 }
 function Quinta(){
   return (
   <View>
     <Text>Quinta</Text>
+    <Text>Almoço</Text>
+    <Text>Principal</Text>
+    <Text>Assado de Panela Cubos de Frango ao Molho</Text>
+    <Text>Vegetariano</Text>
+    <Text>Fricassê de Grão de Bico</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Beterraba, Tomate e Abacaxi</Text>
+    <Text>Guarnição</Text>
+    <Text>Cuscuz</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Cajá</Text>
+    <Text>Sobremesa</Text>
+    <Text>Doce</Text>
+
+    <Text>Jantar</Text>
+    <Text>Principal</Text>
+    <Text>Carne Trinchada  Filé de Frango Grelhado</Text>
+    <Text>Vegetariano</Text>
+    <Text>Almôngedas de Soja</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Alface, Cenoura, Pepino e Milho</Text>
+    <Text>Guarnição</Text>
+    <Text>Macarrão Espaguete</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Manga</Text>
+    <Text>Sobremesa</Text>
+    <Text>Laranja Doce</Text>
   </View>) ;
 }
 function Sexta(){
   return (
   <View>
     <Text>Sexta</Text>
+    <Text>Almoço</Text>
+    <Text>Principal</Text>
+    <Text>Assado de Panela Cubos de Frango ao Molho</Text>
+    <Text>Vegetariano</Text>
+    <Text>Fricassê de Grão de Bico</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Beterraba, Tomate e Abacaxi</Text>
+    <Text>Guarnição</Text>
+    <Text>Cuscuz</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Cajá</Text>
+    <Text>Sobremesa</Text>
+    <Text>Doce</Text>
+
+    <Text>Jantar</Text>
+    <Text>Principal</Text>
+    <Text>Carne Trinchada  Filé de Frango Grelhado</Text>
+    <Text>Vegetariano</Text>
+    <Text>Almôngedas de Soja</Text>
+    <Text>Salada</Text>
+    <Text>Acelga, Alface, Cenoura, Pepino e Milho</Text>
+    <Text>Guarnição</Text>
+    <Text>Macarrão Espaguete</Text>
+    <Text>Acompanhamentos</Text>
+    <Text>Arroz Branco Arroz Integral Feijão de Corda</Text>
+    <Text>Suco</Text>
+    <Text>Manga</Text>
+    <Text>Sobremesa</Text>
+    <Text>Laranja Doce</Text>
   </View>) ;
 }
 
@@ -84,7 +238,7 @@ var menu = StyleSheet.create({
     padding: '0.1%',
   },
   botao:{
-    backgroundColor:'skyblue',
+    backgroundColor:'red',
   }
   
 });
