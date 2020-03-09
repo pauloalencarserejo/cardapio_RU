425143b21c7cdf740710f37c8489ffe3fd960b50
georgemasullo <34112779+georgemasullo@users.noreply.github.com>
2020-03-09 20:32:25 -0300

    Atualização

--- src/diaDaSemana.js
+++ src/diaDaSemana.js
@@ -1,68 +1,127 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, ImageBackground,ScrollView  } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, ScrollView } from 'react-native';
 
 export default function diaDaSemana(props) {
 
     return (
         <ImageBackground source={require('./../assets/img/fundo6.jpg')} style={styles.backgroundImage}  >
-            <View >
-                <ScrollView>
-                <View>
-                    <Text>Segunda</Text>
-                    <View>
-                        <Text>Almoço</Text>
+            <ScrollView style={styles.conteiner}>
+                <View style={styles.coluna}>
+                    <Text style={styles.diaDaSemana}>Segunda</Text>
+                    <View >
                         <View>
-                            <Text>Principal</Text>
+                        <Text>Almoço</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Principal: </Text>
                             <Text>{props.almocoPrincipal}</Text>
-                            <Text>Vegetariano</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Vegetariano: </Text>
                             <Text>{props.almocoVegetariano}</Text>
-                            <Text>Salada</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Salada: </Text>
                             <Text>{props.almocoSalada}</Text>
-                            <Text>Guarnição</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Guarnição: </Text>
                             <Text>{props.almocoGuarnicao}</Text>
-                            <Text>Acompanhamentos</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Acompanhamentos: </Text>
                             <Text>{props.almocoAcompanhamento}</Text>
-                            <Text>Suco</Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View>
+                            <Text>Suco: </Text>
                             <Text>{props.almocoSuco}</Text>
-                            <Text>Sobremesa</Text>
+                            </View>
+                            <View>
+                            <Text>Sobremesa: </Text>
                             <Text>{props.almocoSobremesa}</Text>
                         </View>
+                        </View>
+
                     </View>
+
                     <View>
-                        <Text>Jantar</Text>
                         <View>
-                        <Text>Principal</Text>
+                        <Text>Jantar</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Principal: </Text>
                             <Text>{props.jantaPrincipal}</Text>
-                            <Text>Vegetariano</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Vegetariano: </Text>
                             <Text>{props.jantaVegetariano}</Text>
-                            <Text>Salada</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Salada: </Text>
                             <Text>{props.jantaSalada}</Text>
-                            <Text>Guarnição</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Guarnição: </Text>
                             <Text>{props.jantaGuarnicao}</Text>
-                            <Text>Acompanhamentos</Text>
+                        </View>
+                        <View style={styles.linha}>
+                            <Text>Acompanhamentos: </Text>
                             <Text>{props.jantaAcompanhamento}</Text>
-                            <Text>Suco</Text>
+                        </View>
+                        <View style={styles.item}>
+                            <View>
+                            <Text>Suco: </Text>
                             <Text>{props.jantaSuco}</Text>
-                            <Text>Sobremesa</Text>
+                            </View>
+                            <View>
+                            <Text>Sobremesa: </Text>
                             <Text>{props.jantaSobremesa}</Text>
                         </View>
+                        </View>
+
                     </View>
                 </View>
-                </ScrollView>
-            </View>
+            </ScrollView>
         </ImageBackground>
     );
 
 }
 
 var styles = StyleSheet.create({
-    backgroundImage: {
-      resizeMode: 'center',
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      width: '100%',
-      height: '100%',  
+    cab:{
+       
     },
-  });
+    backgroundImage: {
+        resizeMode: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+    },
+    conteiner: {
+        padding: '10%',
+    },
+    coluna: {
+        paddingRight:'10%',
+        width:'86%',
+        
+    },
+    item:{
+            flexDirection:'row',
+            justifyContent:'space-evenly',
+    },
+    diaDaSemana: {
+        textAlign: 'center',
+        color: '#DF0101',
+        fontSize: 15,
+    },
+    linha: {
+        flexDirection: 'row',
+        paddingRight:'25%',
+        width:'90%',
+        
+
+    }
+});
